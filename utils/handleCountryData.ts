d6d71699e554a50ccbef97ce8f1332fa77d9873b
Marcus Vinícius <marcusvinici25@gmail.com>
2023-05-11 21:05:04 -0300

    creating utils for get CountryData

---
+++ utils/handleCountryData.ts
@@ -0,0 +1,83 @@
+import bangalore from "../icons/countries/bangalore-india.jpeg";
+import barcelona from "../icons/countries/Barcelona-spain.jpg";
+import houston from "../icons/countries/houston-usa.jpg";
+import kolkata from "../icons/countries/kolkata-india.jpeg";
+import lisbon from "../icons/countries/lisbon-pt.jpg";
+import london from "../icons/countries/london-uk.jpg";
+import newyork from "../icons/countries/newyork-usa.jpg";
+import riodejaneiro from "../icons/countries/riodejaneiro.jpg";
+import sanfrancisco from "../icons/countries/san-francisco-usa.jpg";
+import seoul from "../icons/countries/seoul-korea.jpg";
+
+export const CountryData = [
+  {
+    country: "india",
+    city: "Bangalore",
+    img: bangalore,
+    title: "Hackathon in Bangalore, India",
+    description: `Engage in hackathons in Bangalore, India's tech capital. Known as the Silicon Valley of India, this city hosts numerous hackathons that bring together talented developers, designers, and entrepreneurs. Join the vibrant tech community and showcase your skills in Bangalore hackathon competitions.`,
+  },
+  {
+    country: "india",
+    city: "Kolkata",
+    img: kolkata,
+    title: "Hackathon in Kolkata, India",
+    description: `Explore the vibrant hackathon scene in Kolkata, India. This cultural capital is embracing technology and innovation, attracting enthusiasts and startups from various domains. Drink in the cultural mix of Kolkata and unleash your creativity and problem-solving skills.`,
+  },
+  {
+    country: "spain",
+    city: "Barcelona",
+    img: barcelona,
+    title: "Hackathons in Barcelona, Spain",
+    description: `Experience the vibrant hackathon scene in Barcelona, Spain. This cosmopolitan city is known for its creativity and entrepreneurial spirit, mixed with fun and diversity. From the “Sagrada Família” curch or “La Ramblas”, here you can engage in hackathons that bring together talented individuals and foster innovation like no other.`,
+  },
+  {
+    country: "portugal",
+    city: "Lisbon",
+    img: lisbon,
+    title: "Hackathons in Lisbon, Portugal",
+    description: `Discover the thriving hackathon ecosystem in Lisbon, Portugal. This historic city is rapidly becoming a hotspot for tech enthusiasts and entrepreneurs. Grab your “pastel de nata” and experience the blend of traditional charm and technological innovation in Lisbon.`,
+  },
+  {
+    country: "usa",
+    city: "San Francisco",
+    img: sanfrancisco,
+    title: "Hackathons in San Francisco, California, USA",
+    description: `Immerse yourself in the tech mecca of the world with hackathons in San Francisco, California. Home to renowned startups and tech giants, this city offers a dynamic environment for innovation. No better place to unleash your potential and join the vibrant hackathon community in San Francisco.`,
+  },
+  {
+    country: "united kingdom",
+    city: "London",
+    img: london,
+    title: "Hackathons in London, United Kingdom",
+    description: `As a global financial hub, London is also a thriving center for technology and startups. Engage with the innovative spirit of the city by participating in hackathons that fuel creativity and collaboration while overlooking the Thames and your Majesty.`,
+  },
+  {
+    country: "usa",
+    city: "Houston",
+    img: houston,
+    title: "Hackathons in Houston, Texas, USA",
+    description: `Join the exciting hackathon community in Houston, Texas. This city is a melting pot of industries, including energy, healthcare, and aerospace. Participate in hackathons that leverage Houston's expertise to solve complex problems and drive technological advancements. To the moon!`,
+  },
+  {
+    country: "usa",
+    city: "New York",
+    img: newyork,
+    title: "Hackathons in New York, New York, USA",
+    description: `Engage in the vibrant hackathon scene of the city that never sleeps. New York, a global cultural and financial hub, offers a diverse range of hackathons where innovators from around the world converge. Join the thriving tech community and be part of the city's rich cultural tapestry as you collaborate on innovative solutions at hackathons in New York. The concrete jungle is yours to take. New York, New York!`,
+  },
+  {
+    country: "south korea",
+    city: "Seoul",
+    img: seoul,
+    title: "Hackathons in Seoul, South Korea",
+    description: `Immerse yourself in the dynamic hackathon culture of Seoul, South Korea. This futuristic city blends technology, culture, and innovation seamlessly. Participate in hackathons that showcase Seoul's tech prowess and collaborate with like-minded individuals to shape the future. Annyeong Haseyo!`,
+  },
+  {
+    country: "brazil",
+    city: "Rio de Janeiro",
+    img: riodejaneiro,
+    title: "Hackathons in Rio de Janeiro, Brazil",
+    description: `Embark on a hackathon journey in the “Cidade Maravilhosa” Rio de Janeiro, Brazil. Known for its vibrant culture and breathtaking landscapes, this city is also fostering technological advancements through hackathons. Join the community of innovators in Rio and create solutions that make a positive impact.`,
+  },
+];
