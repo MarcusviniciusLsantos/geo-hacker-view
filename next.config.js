3b97c6d99e19d92a32310fa1fbf956b157951093
Marcus Vinícius <marcusvinici25@gmail.com>
2023-05-11 21:01:21 -0300

    adjusting next-config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const publicRuntimeConfig = {
+  urls: {
+    api: process.env.NEXT_PUBLIC_API_HOST || "http://localhost:3000",
+  }
+}
+
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig
 }
 
 module.exports = nextConfig
