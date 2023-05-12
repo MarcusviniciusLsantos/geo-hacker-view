c49d3009dc6a32587a80db61cca161ff771b1928
Marcus Vinícius <marcusvinici25@gmail.com>
2023-05-11 21:03:59 -0300

    creating api service

---
+++ services/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
+
+export const api = axios.create({
+  baseURL: `${publicRuntimeConfig?.urls?.api}/api`
+});
